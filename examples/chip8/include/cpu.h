21ac100b393253cca8e84272fb916885f91c2859
Jeremy Kiel <jeremykiel11@gmail.com>
2023-02-03 23:17:30 +1300

    Added chip 8 system

---
+++ examples/chip8/include/cpu.h
@@ -0,0 +1,7 @@
+#ifndef CPU_H
+#define CPU_H
+
+int cpu_init();
+void cpu_tick(int ms_delta);
+
+#endif
--- premake5.lua
+++ premake5.lua
@@ -210,3 +210,66 @@ project "Starship"
     filter "configurations:release"
 		kind "WindowedApp"
         optimize "On"
+		
+
+------------------------------------------------------------------
+
+
+project "CHIP-8"
+	location "examples/chip8"
+	language "C"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("tmp/" .. outputdir .. "/%{prj.name}")
+
+
+    files {
+        "%{prj.location}/include/**.h",
+		"%{prj.location}/src/**.c"
+    }
+
+
+    includedirs {
+		"%{prj.location}/include/",
+        "engine/include/api"
+    }
+	
+	
+	filter "system:windows"
+		staticruntime "On"
+		systemversion "latest"
+
+        links {
+            "Engine"
+        }
+	
+	
+	filter "system:macosx"
+        buildoptions {
+            "-F /Library/Frameworks"
+        }
+        linkoptions {
+            "-F /Library/Frameworks",
+            "-framework Cocoa",
+            "-framework Foundation"
+        }
+
+        links {
+            "Engine"
+        }
+	
+	
+	filter "system:linux"
+		links {
+            "Engine",
+			"m"
+        }
+	
+
+    filter "configurations:debug"
+		kind "ConsoleApp"
+        symbols "On"
+    
+    filter "configurations:release"
+		kind "WindowedApp"
+        optimize "On"
