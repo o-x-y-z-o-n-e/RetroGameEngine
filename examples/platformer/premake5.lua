325985455f7cd93b7bc98f0658e7ef345a6769c7
Jeremy Kiel <jeremykiel11@gmail.com>
2023-12-23 19:56:24 +1300

    project restructure

---
+++ examples/platformer/premake5.lua
@@ -0,0 +1,56 @@
+project "platformer"
+    language "C++"
+    cppdialect "C++11"
+
+    -- defines "SYS_SOFTWARE_GL"
+    -- defines "SYS_OPENGL_1_0"
+    defines "SYS_OPENGL_3_3"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/tmp/" .. outputdir .. "/%{prj.name}")
+
+    files {
+		"src/**.cpp",
+		"src/**.hpp",
+		"src/**.c",
+		"src/**.h"
+    }
+
+    includedirs {
+		"%{wks.location}/include",
+		"%{wks.location}/vendor"
+    }
+	
+	filter "system:windows"
+		staticruntime "On"
+		systemversion "latest"
+		entrypoint "mainCRTStartup"
+	
+	filter "system:macosx"
+        buildoptions {
+            "-F /Library/Frameworks"
+        }
+        linkoptions {
+            "-F /Library/Frameworks",
+            "-framework Foundation",
+            "-framework Cocoa",
+            "-framework OpenGL"
+        }
+        files {
+            "ext/macosx.mm"
+        }
+	
+	filter "system:linux"
+		links {
+            "m"
+        }
+	
+    filter "configurations:debug"
+		kind "ConsoleApp"
+		runtime "Debug"
+        symbols "On"
+    
+    filter "configurations:release"
+		kind "WindowedApp"
+		runtime "Release"
+        optimize "On"
